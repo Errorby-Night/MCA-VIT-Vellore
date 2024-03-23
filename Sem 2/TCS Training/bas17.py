ba234be257adb517c32714e9c6e76fe7aea7b3d3
Srijan <srijandutta200@gmail.com>
2024-03-23 10:42:49 +0530

    base 177

--- Sem 2/TCS Training/bas17.py
+++ Sem 2/TCS Training/bas17.py
@@ -1,4 +1,5 @@
 dic_vals = {10: 'A', 11: 'B', 12: 'C', 13:'D', 14:'E', 15: 'F', 16:'G'}
+rev_dic_vals = {'A': 10, 'B':11, 'C': 12, 'D':13, 'E': 14, 'F': 15, 'G': 16}
 
 def calc(n):
     s = ''
@@ -8,12 +9,27 @@ def calc(n):
             x = dic_vals[x]
         s += str(x)
         n //=17
-        
+
     return s[::-1]
 
+
+def revcalc(s):
+    n = 0
+    x = list(s)
+    i = len(x) - 1
+    j = i
+    while(i >= 0):
+        pow = 17 ** (j - i)
+        c = x[i]
+        if c in rev_dic_vals:
+            n =  n + rev_dic_vals[c] * pow
+        else:
+            n = n + int(c) * pow
+        i -= 1
+    return n
 def main():
-    num = int(input("Enter a number: "))
-    print('Result: ',calc(num))
+    num = input("Enter a number: ")
+    print('Result: ',revcalc(num))
     
 if __name__ == '__main__':
     main()
