684b14d21684f8d4edff0efdac5f627b214e6af1
Srijan <srijandutta200@gmail.com>
2024-03-23 10:31:37 +0530

    base of 17

---
+++ Sem 2/TCS Training/bas17.py
@@ -0,0 +1,18 @@
+dic_vals = {10: 'A', 11: 'B', 12: 'C', 13:'D', 14:'E', 15: 'F', 16:'G'}
+
+def calc(n):
+    s = ''
+    while(n > 0):
+        x = n % 17
+        if(x > 9):
+            x = dic_vals[x]
+        s += str(x)
+        n //=17
+    return s[::-1]
+
+def main():
+    num = int(input("Enter a number: "))
+    print('Result: ',calc(num))
+    
+if __name__ == '__main__':
+    main()
